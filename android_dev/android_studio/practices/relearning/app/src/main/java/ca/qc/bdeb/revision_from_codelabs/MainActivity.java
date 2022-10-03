dfd3bbebafb97f406fa68d59ff65c84cd734fd06
Alex Kao <ab.kao@hotmail.com>
2022-10-02 22:59:38 -0400

    Created an incrementation button

--- android_dev/android_studio/practices/relearning/app/src/main/java/ca/qc/bdeb/revision_from_codelabs/MainActivity.java
+++ android_dev/android_studio/practices/relearning/app/src/main/java/ca/qc/bdeb/revision_from_codelabs/MainActivity.java
@@ -4,15 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int mCount = 0;
+    private TextView mShowCount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d("test", "Hello World");
+        mShowCount = (TextView) findViewById(R.id.show_count);
     }
 
     public void showToast(View view) {
@@ -21,5 +26,13 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void increaseCount(View view) {
+        mCount++;
+        if (mShowCount != null) {
+            mShowCount.setText(Integer.toString(mCount));
+            Log.d("checkingCount", "The count has increased.");
+        } else {
+            Log.d("checkingCount", "mShowCount is null");
+        }
+
     }
 }
