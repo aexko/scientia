cf9c8614e3f9046d28244a7e1de80214034d9878
Alex Kao <alexkxosb@gmail.com>
2022-10-07 12:14:59 -0400

    Updating output with user's input (editText)

--- android_dev/android_studio/practices/relearning/app/src/main/java/ca/qc/bdeb/revision_from_codelabs/MainActivity.java
+++ android_dev/android_studio/practices/relearning/app/src/main/java/ca/qc/bdeb/revision_from_codelabs/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -11,6 +12,8 @@ public class MainActivity extends AppCompatActivity {
 
     private int mCount = 0;
     private TextView mShowCount;
+    private TextView username;
+    private EditText inputUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,11 +21,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Log.d("test", "Hello World");
         mShowCount = (TextView) findViewById(R.id.show_count);
+        inputUser = (EditText) findViewById(R.id.editTxt_username);
+        username = (TextView) findViewById(R.id.username1);
     }
 
     public void showToast(View view) {
         Toast toast = Toast.makeText(this, R.string.toast_message, Toast.LENGTH_SHORT);
         toast.show();
+        Log.d("checkingToast", "The toast showed up");
     }
 
     public void increaseCount(View view) {
@@ -33,6 +39,10 @@ public class MainActivity extends AppCompatActivity {
         } else {
             Log.d("checkingCount", "mShowCount is null");
         }
+    }
 
+    public void changeName(View view) {
+        username.setText(inputUser.getText());
+        Log.d("checkingChangingName", "The user entered: " + inputUser.getText().toString());
     }
 }
