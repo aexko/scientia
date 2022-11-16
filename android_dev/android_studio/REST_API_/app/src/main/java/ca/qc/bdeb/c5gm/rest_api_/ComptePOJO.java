ba5d7d60485163a8772cf7b7d168e6c638ce30ae
Alex Kao <alexkxosb@gmail.com>
2022-11-16 17:04:55 -0500

    REST APIs with Android

---
+++ android_dev/android_studio/REST_API_/app/src/main/java/ca/qc/bdeb/c5gm/rest_api_/ComptePOJO.java
@@ -0,0 +1,18 @@
+package ca.qc.bdeb.c5gm.rest_api_;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+public class ComptePOJO implements Serializable {
+        @SerializedName("id")
+        private UUID uuid;
+        // @SerializedName("nom")
+        private String nom;
+        // @SerializedName("prenom")
+        private String prenom;
+        // @SerializedName("email")
+        private String email;
+
+}
