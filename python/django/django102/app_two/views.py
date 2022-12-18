08e9063423f3dba86ab86f957fdd05c29e09f819
Alex Kao <alexkxosb@gmail.com>
2022-12-18 14:45:33 -0500

    UDEMY: 45

--- python/django/django102/app_two/views.py
+++ python/django/django102/app_two/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 from django.template import loader
+
+
 # Create your views here.
 def djangorocks(request):
     return HttpResponse('This is a Jazzy Reponse')
@@ -9,4 +11,20 @@ def djangorocks(request):
 
 def pictures_detail(request, category, year=0, month=0, day=0):
     template = loader.get_template('app_two/index.html')
-    return HttpResponse(template.render({}, request))
+
+    picture = {
+        'filename': 'moi.png',
+        'categories': ['color', 'landscape',],
+    }
+
+    context = {
+        'title': 'TITLEEEE',
+        'category': category,
+        'year': year,
+        'month': month,
+        'day': day,
+        'picture': picture,
+    }
+    return HttpResponse(template.render(context, request))
+
+
