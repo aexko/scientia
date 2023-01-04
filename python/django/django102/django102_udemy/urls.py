4091afe23b9f2db3217080d1d3b217c84039f610
Alex Kao <alexkxosb@gmail.com>
2023-01-04 17:29:17 -0500

    UDEMY: 56

--- python/django/django102/django102_udemy/urls.py
+++ python/django/django102/django102_udemy/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('hello/', app_one_views.hello),
     path('app_two/', include('app_two.urls')),
-    path('peoplebook/', include('peoplebook.urls'))
-
+    path('peoplebook/', include('peoplebook.urls')),
+    path('songs/', app_one_views.song_list),
+    path('songs/add/<str:song_name>/<int:duration>/', app_one_views.songs_add)
 ]
