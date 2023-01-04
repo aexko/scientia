78b0d2d66cab140866a001a43f26a49a74a16fb6
Alex Kao <alexkxosb@gmail.com>
2023-01-04 16:37:29 -0500

    UDEMY: 52-53

--- python/django/django102/app_one/models.py
+++ python/django/django102/app_one/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
+
 # Create your models here.
+class Song(models.Model):
+    # pas besoin de faire ca puisque django le fait automatiquement
+    # id = models.AutoField
+    name = models.CharField(max_length=255, default='No name')
+    duration = models.IntegerField(default=0, help_text='Duration in seconds')
+    lyrics = models.TextField(blank=True)
+
+    # definir ca pour que ca soit plus representatif au lieu d'une simple adresse qui veut rien dire
+    # pk est le id, on peut utiliser id, mais il vaut mieux utiliser pk
+    def __str__(self):
+        return self.pk
+
