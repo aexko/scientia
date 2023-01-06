4838ad5b4bc680ad30a2c053951f3395410a3d6a
Alex Kao <alexkxosb@gmail.com>
2023-01-06 16:37:57 -0500

    UDEMY: 59*-62-63

--- python/django/django102/app_one/models.py
+++ python/django/django102/app_one/models.py
@@ -6,6 +6,9 @@ class Song(models.Model):
     # pas besoin de faire ca puisque django le fait automatiquement
     # id = models.AutoField
     name = models.CharField(max_length=255, default='No name')
+    album = models.ForeignKey('Album',
+                              on_delete=models.CASCADE,
+                              default='0')
     duration = models.IntegerField(default=0, help_text='Duration in seconds')
     lyrics = models.TextField(blank=True)
 
@@ -14,3 +17,11 @@ class Song(models.Model):
     def __str__(self):
         return '{} {}'.format(self.name, self.duration)
 
+
+class Album(models.Model):
+    name = models.CharField(max_length=255)
+    artist_name = models.CharField(max_length=255)
+    release_date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return '{} - {}'.format(self.name, self.artist_name)
