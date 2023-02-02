ac9371ba4764548caef2749dc9de7e86564013e2
Alex Kao <alexkxosb@gmail.com>
2023-02-02 16:02:42 -0500

    Creation de django

--- web_dev/vue/vue_django_rest/django_rest_api/django_rest_api/urls.py
+++ web_dev/vue/vue_django_rest/django_rest_api/django_rest_api/urls.py
@@ -1,4 +1,4 @@
-"""django_rest_api_plantes URL Configuration
+"""django_rest_api URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
