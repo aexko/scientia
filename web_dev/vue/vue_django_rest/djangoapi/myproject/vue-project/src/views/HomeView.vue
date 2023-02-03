6fafe7d0894ee3b552eeabb9a71267af3702feb3
Alex Kao <alexkxosb@gmail.com>
2023-02-03 17:18:50 -0500

    Installation de Vuetify

--- web_dev/vue/vue_django_rest/djangoapi/myproject/vue-project/src/views/HomeView.vue
+++ web_dev/vue/vue_django_rest/djangoapi/myproject/vue-project/src/views/HomeView.vue
@@ -1,5 +1,18 @@
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <HelloWorld />
 </template>
+
+<script>
+import { defineComponent } from 'vue';
+
+// Components
+import HelloWorld from '../components/HelloWorld.vue';
+
+export default defineComponent({
+  name: 'HomeView',
+
+  components: {
+    HelloWorld,
+  },
+});
+</script>
