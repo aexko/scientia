eb524c2e81320aff33d0a9a3c23cb73a601570ee
Alex Kao <alexkxosb@gmail.com>
2023-02-10 10:54:23 -0500

    ok

--- web_dev/vue/vue_django_rest/djangoapi/myproject/vue-project/src/components/HelloWorld.vue
+++ web_dev/vue/vue_django_rest/djangoapi/myproject/vue-project/src/components/HelloWorld.vue
@@ -6,6 +6,7 @@
       </v-col>
 
       <v-col class="mb-4">
+        <v-alert type="success"></v-alert>
         <h1 class="display-2 font-weight-bold mb-3">
           Welcome to the Vuetify 3 Beta
         </h1>
