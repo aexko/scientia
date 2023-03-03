4a438de156a4927f3d0da70e10a2ad843f1187b6
Alex Kao <alexkxosb@gmail.com>
2023-03-03 10:46:04 -0500

    Object level permissions

--- web_dev/django/full_drf_tutorial/tutorial/tutorial/urls.py
+++ web_dev/django/full_drf_tutorial/tutorial/tutorial/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path, include
+from django.contrib.admin import site
 
 urlpatterns = [
+    path("admin/", site.urls),
     path("", include("snippets.urls")),
+]
+urlpatterns += [
+    path("api-auth/", include("rest_framework.urls")),
 ]
