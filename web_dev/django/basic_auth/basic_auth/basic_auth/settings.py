4bcfed7e541407245dfe4a484263c0552f3a5a06
Alex Kao <alexkxosb@gmail.com>
2023-03-10 09:56:55 -0500

    feat: cors_allowed_origins

--- web_dev/django/basic_auth/basic_auth/basic_auth/settings.py
+++ web_dev/django/basic_auth/basic_auth/basic_auth/settings.py
@@ -1,3 +1,6 @@
+
+CORS_ALLOWED_ORIGINS = ["http://localhost:8000", "http://127.0.0.1:8000"]
+
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework.authentication.TokenAuthentication",
