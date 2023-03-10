55e53e61438400adb57677b4934955c710ce74d0
Alex Kao <alexkxosb@gmail.com>
2023-03-10 10:52:35 -0500

    Create App.vue

---
+++ web_dev/django/basic_auth/vue_basic_auth/src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+</script>
+
+<template>
+	<div id="app">
+  <p>
+    <!-- use the router-link component for navigation. -->
+    <!-- specify the link by passing the `to` prop. -->
+    <!-- `<router-link>` will render an `<a>` tag with the correct `href` attribute -->
+    <router-link to="/login">Go to LOGIN</router-link><br>
+    <router-link to="/signup">Go to SINUP</router-link>
+  </p>
+  <!-- route outlet -->
+  <!-- component matched by the route will render here -->
+  <router-view></router-view>
+</div>
+</template>
+
