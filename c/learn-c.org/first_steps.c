1f7da3c0a08fae6e09b610fef0a41545dbba6960
Alex Kao <alexkxosb@gmail.com>
2023-04-28 17:14:28 -0400

    Variables and Types

--- c/learn-c.org/first_steps.c
+++ c/learn-c.org/first_steps.c
@@ -2,5 +2,14 @@
 
 int main()
 {
-    printf("Hello world!!");
+    int a = 0, b = 1, c = 2, d = 3, e = 4;
+    a = b - c + d * e;
+
+    // ca le fait sans le % a la fin
+    printf("%d\n", a);
+    // ca le fait avec le % a la fin
+    printf("%d", a);
+
+    // toujours mettre return 0
+    return 0;
 }
